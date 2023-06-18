ba84c3ff86ed22e22d935463c91fe3fb9673876e
Katrin Schmid <3a1b2c3@protonmail.com>
2023-06-18 12:21:04 +1000

    pages

---
+++ pages/page.jsx
@@ -0,0 +1,13 @@
+import Chat from "./components/Chat";
+
+export const metadata = {
+  title: "Chat with short-term memory",
+};
+
+export default function Home() {
+  return (
+    <main>
+      <Chat />
+    </main>
+  );
+}
